8eccf9f2e4e83d2abb93e45c31ce08ea79939167
aomsnnn <omsinjah@gmail.com>
2023-01-13 16:01:27 +0700

    lab14.1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,38 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[])
+{
+    double sum,max,min,dev,mul;
+    int i;
+    for(i=0;i<n;i++) sum += a[i];
+    b[0] = sum/n;
+
+    dev = 0;
+    for(i=0;i<n;i++) dev += pow((a[i]-b[0]),2);
+    b[1] = sqrt(dev/n);
+
+    mul = 1;
+    for(i=0;i<n;i++) mul = mul*a[i];
+    b[2] = pow(mul,(1.0/n));
+
+    sum = 0;
+    for(i=0;i<n;i++) sum += (1/a[i]);
+    b[3] = n/sum;
+
+    max=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max) max = a[i];
+    }
+    b[4] = max;
+    
+    min=a[1];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<min) min = a[i];
+    }
+    b[5] = min;
+
+}
